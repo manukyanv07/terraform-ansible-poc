bed5efc9c1f5945d6db0d733cac13b14e7ffdcfa
Vahram Manukyan <manukyanv07@gmail.com>
2019-11-04 13:29:30 -0300

    destroy variable fix

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,8 +2,24 @@ provider "aws" {
   region = "eu-west-1"
 }
 
+data "aws_ami" "ubuntu" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-trusty-14.04-amd64-server-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  owners = ["099720109477"] # Canonical
+}
+
 resource "aws_instance" "web" {
-  ami           = "ami-08c3796e1e14b07f8"
+  ami           = "${data.aws_ami.ubuntu.id}"
   instance_type = "t2.micro"
 
   tags = {
