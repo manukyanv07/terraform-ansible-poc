61bb0246a7c6cca9b60eb9dfd0f30c6daf3c6956
Vahram Manukyan <manukyanv07@gmail.com>
2019-11-04 12:50:29 -0300

    destroy variable fix

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,11 +2,6 @@ provider "aws" {
   region = "eu-west-1"
 }
 
-data "aws_security_group" "default" {
-  name   = "default"
-  vpc_id = module.vpc.vpc_id
-}
-
 resource "aws_instance" "web" {
   ami           = "ami-08c3796e1e14b07f8"
   instance_type = "t2.micro"
